1eef6652b95d3675a04923fb677d72a5f262a813
Manthan-Gohil <manthangohil22577@gmail.com>
2025-06-02 11:06:10 +0530

    set up the frontend

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full h-screen flex items-center justify-center bg-zinc-900 text-white text-2xl'>
+      Careerwill
+    </div>
+  )
+}
+
+export default App
